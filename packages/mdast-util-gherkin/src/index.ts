1410b26f5b8012718a40dfba9fc19aa39169f224
MasuqaT <occar@hotmail.co.jp>
2026-05-17 03:35:33 +0900

    split type defs

--- packages/mdast-util-gherkin/src/index.ts
+++ packages/mdast-util-gherkin/src/index.ts
@@ -1,36 +1,6 @@
-import type { Literal, Node } from "mdast";
 import type { Extension as FromMarkdownExtension } from "mdast-util-from-markdown";
 import type { Options as ToMarkdownExtension } from "mdast-util-to-markdown";
 import gherkinTransform from "./gherkinTransform.ts";
-import {
-  GHERKIN_SEGMENT_KEYWORD_TYPE,
-  GHERKIN_DELIMITED_PARAMETER_TYPE,
-  GHERKIN_STEP_KEYWORD_TYPE,
-  GHERKIN_TAG_TYPE,
-} from "./constant.ts";
-
-export interface GherkinTagKeyword extends Literal {
-  type: typeof GHERKIN_TAG_TYPE;
-}
-export interface GherkinSegmentKeyword extends Literal {
-  type: typeof GHERKIN_SEGMENT_KEYWORD_TYPE;
-}
-export interface GherkinStepKeyword extends Literal {
-  type: typeof GHERKIN_STEP_KEYWORD_TYPE;
-}
-export interface GherkinDelimitedParameter extends Node {
-  type: typeof GHERKIN_DELIMITED_PARAMETER_TYPE;
-  ident: string;
-}
-
-declare module "mdast" {
-  interface PhrasingContentMap {
-    gherkinTagKeyword: GherkinTagKeyword;
-    gherkinSegmentKeyword: GherkinSegmentKeyword;
-    gherkinStepKeyword: GherkinStepKeyword;
-    gherkinDelimitedParameter: GherkinDelimitedParameter;
-  }
-}
 
 export function gherkinFromMarkdown(): FromMarkdownExtension {
   return {
